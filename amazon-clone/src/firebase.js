a683414f615f4b617d5100fecd7f6cddfef0d6ea
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-09-04 13:14:25 +0300

    home page design is completed

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBy4NL0XRLdy_D65E56S12kLqNw7OhI-t4",
+    authDomain: "clone-438f2.firebaseapp.com",
+    projectId: "clone-438f2",
+    storageBucket: "clone-438f2.appspot.com",
+    messagingSenderId: "24110173443",
+    appId: "1:24110173443:web:73681bf4ac9005d651ad36",
+    measurementId: "G-6CPFXS63FD"
+  };
